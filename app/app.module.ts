d7daee04f18c6d6a43aa6b2e55ae373715a8a4c7
Lucas Barbula <lucas.barbula@gmail.com>
2018-01-06 13:30:10 -0700

    setting up first component and module

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {EventsAppComponent} from './events-app.component.ts'
+
+@NgModule({
+  imports:  [BrowserModule],
+  components: [EventsAppComponent]
+})
+class AppModule {
+
+}
+export AppModule;
---
+++ app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: "<h2>Hello World!</h2>"
+})
+class EventsAppComponent {
+
+}
+
+export EventsAppComponent;
